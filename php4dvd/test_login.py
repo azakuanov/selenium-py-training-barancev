60674da579d8b32ff2fdcd7bca0d134fd7f882ea
barancev <barancev@gmail.com>
2013-04-12 19:48:58 +0400

    Fixed compilation errors and style

--- php4dvd/test_login.py
+++ php4dvd/test_login.py
@@ -1,18 +1,18 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
-import unittest, time, re
+from selenium.common.exceptions import *
+import unittest
+
 
 class Untitled(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Firefox(
+            capabilities={'native_events': False})
         self.driver.implicitly_wait(10)
         self.base_url = "http://localhost/"
         self.verificationErrors = []
         self.accept_next_alert = True
-    
-    def test_untitled(self):
+
+    def test_login(self):
         driver = self.driver
         driver.get(self.base_url + "/php4dvd/")
         driver.find_element_by_id("username").clear()
@@ -20,12 +20,14 @@ class Untitled(unittest.TestCase):
         driver.find_element_by_name("password").clear()
         driver.find_element_by_name("password").send_keys("admin")
         driver.find_element_by_name("submit").click()
-    
+
     def is_element_present(self, how, what):
-        try: self.driver.find_element(by=how, value=what)
-        except NoSuchElementException, e: return False
+        try:
+            self.driver.find_element(how, what)
+        except NoSuchElementException, e:
+            return False
         return True
-    
+
     def close_alert_and_get_its_text(self):
         try:
             alert = self.driver.switch_to_alert()
@@ -34,8 +36,9 @@ class Untitled(unittest.TestCase):
             else:
                 alert.dismiss()
             return alert.text
-        finally: self.accept_next_alert = True
-    
+        finally:
+            self.accept_next_alert = True
+
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
