cb0b48b3c21504cb71a93a3c05eaca84f858a030
barancev <barancev@gmail.com>
2013-04-13 16:00:18 +0400

    Implementing Test Self-Care

--- php4dvd/test_login.py
+++ php4dvd/test_login.py
@@ -3,14 +3,14 @@ from model.user import User
 
 
 def test_login_with_invalid_credentials(app):
+    app.ensure_logout()
     app.login(User.random())
     assert app.is_not_logged_in()
 
 
 def test_login_with_valid_credentials(app):
+    app.ensure_logout()
     app.login(User.Admin())
     assert app.is_logged_in()
-    app.logout()
-    assert app.is_not_logged_in()
 
 
