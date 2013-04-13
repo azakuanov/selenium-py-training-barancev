c67ac86bdc4b81c3fd3ad53651fd806227dcd527
barancev <barancev@gmail.com>
2013-04-13 13:02:09 +0400

    1) Cleaning, 2) Waiting for alert

--- php4dvd/model/application.py
+++ php4dvd/model/application.py
@@ -1,21 +1,19 @@
 from php4dvd.pages.internal_page import InternalPage
 from php4dvd.pages.login_page import LoginPage
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.expected_conditions import *
 
 
 class Application(object):
     def __init__(self, driver, base_url):
-        self.driver = driver
         driver.get(base_url)
+        self.wait = WebDriverWait(driver, 10)
         self.login_page = LoginPage(driver, base_url)
         self.internal_page = InternalPage(driver, base_url)
-        self.wait = WebDriverWait(driver, 10)
 
     def logout(self):
         self.internal_page.logout_button.click()
-        self.driver.switch_to_alert().accept()
+        self.wait.until(alert_is_present()).accept()
 
     def login(self, user):
         lp = self.login_page
