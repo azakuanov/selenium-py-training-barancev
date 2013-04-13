c7abb0551c39e49c27b025aa15f4f67624954e0a
barancev <barancev@gmail.com>
2013-04-13 10:42:50 +0400

    Adding another test, fixing fixture to start the browser once

--- php4dvd/test_login.py
+++ php4dvd/test_login.py
@@ -4,7 +4,12 @@ from selenium.common.exceptions import *
 from selenium_fixture import app
 
 
-def test_login(app):
+def test_login_with_valid_credentials(app):
     app.go_to_home_page()
     app.login(User.Admin())
     app.logout()
+
+
+def test_login_with_invalid_credentials(app):
+    app.go_to_home_page()
+    app.login(User.random())
